d993746f5d2efddbc65440cedae7da5ec44ff280
DaanThai02 <149707458+DaanThai02@users.noreply.github.com>
2025-02-06 22:13:46 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,39 @@
-console.log("hello");
+// noise //
+const canvas = document.getElementById('noise');
+const ctx = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+function drawNoise() {
+    const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    const data = imageData.data;
+    for (let i = 0; i < data.length; i += 4) {
+        const color = Math.random() * 255;
+        data[i] = color;
+        data[i + 1] = color;
+        data[i + 2] = color;
+        data[i + 3] = 255;
+    }
+    ctx.putImageData(imageData, 0, 0);
+    requestAnimationFrame(drawNoise);
+}
+
+drawNoise();
+
+// Add class to animate
+// setTimeout(() => {
+//     canvas.classList.add('noise-animate');
+// }, 0);
+
+
+
+
+console.log("Hi, welcome to my portfolio!");
+
+
+
+
 
 document.querySelectorAll("img").forEach(image => {
     image.onclick = () =>{
@@ -42,8 +77,4 @@ window.addEventListener("scroll", function(event){
        el.classList.remove("button_fade_up");
     }
 })
-
-
-
-
 
